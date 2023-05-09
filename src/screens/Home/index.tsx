add0ddefd7a9e927983c3fb852c3780ed37a00ec
Bruno Gomes <bgbf@ic.ufal.br>
2023-05-09 18:00:56 -0300

    wip: bottom sheet modal

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,66 @@
-import React from 'react';
+import React, {useCallback, useMemo, useRef} from 'react';
 import {MainView} from './styles';
 import {Chart} from '../../components/Chart';
+import BottomSheet, {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet';
+import {StyleSheet, Text, View, Button} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 export const Home = (): JSX.Element => {
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-    datasets: [
-      {
-        data: [20, 45, 28, 80, 99, 43],
-      },
-    ],
-  };
+  const [isOpen, setIsOpen] = React.useState(false);
+  // ref
+  const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
+  // variables
+  const snapPoints = useMemo(() => ['25%', '50%', '75%'], []);
+
+  // callbacks
+  const handlePresentModalPress = useCallback(() => {
+    bottomSheetModalRef.current?.present();
+  }, []);
+  const handleSheetChanges = useCallback((index: number) => {
+    index >= 0 && setIsOpen(prev => prev !== true && true);
+    index < 0 && setIsOpen(prev => prev !== false && false);
+    console.log('handleSheetChanges', index);
+  }, []);
+
+  // renders
   return (
-    <MainView>
-      <Chart data={data} />
-    </MainView>
+    <GestureHandlerRootView
+      style={[
+        styles.container,
+        {backgroundColor: isOpen ? 'rgba(0, 0, 0, 0.5)' : 'white'},
+      ]}>
+      <BottomSheetModalProvider>
+        <Button
+          onPress={handlePresentModalPress}
+          title="Present Modal"
+          color="black"
+        />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          onChange={handleSheetChanges}>
+          <View style={styles.contentContainer}>
+            <Text>Awesome 🎉</Text>
+          </View>
+        </BottomSheetModal>
+      </BottomSheetModalProvider>
+    </GestureHandlerRootView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+  },
+  contentContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
